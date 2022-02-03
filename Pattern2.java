0324a1ef644d36f2a291ba23ee265056c65283df
gayatriwaghdarikar <97940219+gayatriwaghdarikar@users.noreply.github.com>
2022-02-03 12:28:54 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,26 @@
+
+/*
+   *
+   **
+   ***
+   ****
+   *****
+*/
+package basic;
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+	     for(i=1;i<=5;i++)                                  
+	     {
+		 for(j=i;j>=1;j--)  //for(j=5;j>=i;j--)
+		    {  
+		    	System.out.print("*");
+		    }
+		    System.out.println();
+		    }
+
+	}
+
+}

23af9bd041f21a1e89ee3e0b7fda379026ce9905
gayatriwaghdarikar <97940219+gayatriwaghdarikar@users.noreply.github.com>
2022-02-03 12:21:39 +0530

    Add files via upload

---
+++ ReverseOrder.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.Scanner;
+
+public class ReverseOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,n;
+		System.out.println("enter number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=n;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+		
+
+	}
+
+}

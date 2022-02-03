836009be5e386b36293d684f1f14fe61b186b213
gayatriwaghdarikar <97940219+gayatriwaghdarikar@users.noreply.github.com>
2022-02-03 12:27:31 +0530

    Add files via upload

---
+++ PrintEvenNo.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Scanner;
+
+public class PrintEvenNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,n;
+		
+		for(i=1;i<=5;i++)//for(i=1;i<=50;i=++)
+		{
+			//if(i<=n)
+			//{
+				 System.out.println(i+1);
+			//}
+		}
+
+
+	}
+
+}
